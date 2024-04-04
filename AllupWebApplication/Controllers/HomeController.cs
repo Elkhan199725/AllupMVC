46bdec00cb63df1e957e80632a3a0029dd835246
Elkhan Mammadli <elkhantma@code.edu.az>
2024-04-04 07:24:26 +0400

    view for layout and home index created

---
+++ AllupWebApplication/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AllupWebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+    }
+}
